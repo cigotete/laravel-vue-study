0076067eaefda670079775064567966348230230
Camilo Figueroa <cigotete@gmail.com>
2023-03-25 18:18:46 -0500

    Added categories to the course detail. A v-if condition was added due a race condition to show category name. This race condition seems a common behavior. This case needs be validated.

--- src/views/courses/CourseDetails.vue
+++ src/views/courses/CourseDetails.vue
@@ -3,6 +3,11 @@
     <h1>Course Details</h1>
     <p>{{ course.title }}</p>
     <p>{{ course.description }}</p>
+
+    <div v-if="course.category">
+      Category: {{ course.category.name }}
+    </div>
+
   </div>
 </template>
 
@@ -18,7 +23,7 @@ export default {
   },
   methods: {
     getCourse() {
-      this.axios.get('http://laravel-study-vue-api-backend.test/api/courses/' + this.$route.params.id )
+      this.axios.get('http://laravel-study-vue-api-backend.test/api/courses/' + this.$route.params.id + '?included=category')
         .then(response => {
           this.course = response.data;
         })
