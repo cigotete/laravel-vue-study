858bbfe17abe65b17838dc2574a41fcac605758b
Camilo Figueroa <cigotete@gmail.com>
2023-03-25 19:39:48 -0500

    Sending data to the API to save data.

--- src/views/courses/CourseCreate.vue
+++ src/views/courses/CourseCreate.vue
@@ -1,29 +1,35 @@
 <template>
   <h1>Create a Course</h1>
   <div>
-    <form>
+    <form @submit.prevent="saveCourse">
       <div>
         <label for="title">Course Title</label>
         <br>
-        <input type="text" id="title" name="title" placeholder="Course Title">
+        <input v-model="course.title" type="text" id="title" name="title" placeholder="Course Title">
       </div>
       <br>
       <div>
         <label for="description">Course Description</label>
         <br>
-        <textarea id="description" cols="30" rows="10" placeholder="Course Description"></textarea>
+        <textarea v-model="course.description" id="description" cols="30" rows="10" placeholder="Course Description"></textarea>
       </div>
       <br>
       <div>
         <label for="category">Category</label>
         <br>
-        <select id="category">
+        <select id="category" v-model="course.category_id">
+          <option value="" selected disabled>Select a Category</option>
           <option v-for="category in categories" :key="'category-' + category.id" :value="category.id">
             {{ category.name }}
           </option>
         </select>
       </div>
+      <br>
+      <div>
+        <button type="submit">Create a Course</button>
+      </div>
     </form>
+    {{  course }}
   </div>
 </template>
 
@@ -31,7 +37,12 @@
 export default {
   data() {
     return {
-      categories: []
+      categories: [],
+      course: {
+        title: '',
+        description: '',
+        category_id: '',
+      }
     }
   },
 
@@ -48,7 +59,21 @@ export default {
         .catch(error => {
           console.log(error);
         });
-    }
+    },
+    saveCourse() {
+      this.axios.post('http://laravel-study-vue-api-backend.test/api/courses', this.course)
+        .then(response => {
+          this.course = {
+            title: '',
+            description: '',
+            category_id: '',
+          };
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
   },
 }
 </script>
