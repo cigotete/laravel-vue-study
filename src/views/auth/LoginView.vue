5e006816ad0c2dbb3d1c7d53272db15e97989837
Camilo Figueroa <cigotete@gmail.com>
2023-03-27 21:44:41 -0500

    Added login form and data object with email and password properties. Also the login method with a temporal implementation.

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1>Login</h1>
+  <div class="container">
+
+    <h1>Login</h1>
+
+    <div class="card mx-auto">
+      <div class="card-body">
+        <form @submit.prevent="login">
+          <div class="form-group mb-3">
+            <label for="email" class="mb-1">Email</label>
+            <input type="email" v-model="email" class="form-control" id="email" placeholder="Enter email">
+          </div>
+          <div class="form-group mb-3">
+            <label for="password" class="mb-1">Password</label>
+            <input type="password" v-model="password" class="form-control" id="password" placeholder="Password">
+          </div>
+          <button class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log('trying to login');
+    }
+  }
 }
 </script>
 
