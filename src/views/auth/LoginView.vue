fddc769343bc3662be3453e57618f2ec20524e4a
Camilo Figueroa <cigotete@gmail.com>
2023-03-27 23:54:34 -0500

    Adding the behavior to get auth data from backend. Burning client_id and client_secret is not a secure method. This is temporal.

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -14,7 +14,9 @@
             <label for="password" class="mb-1">Password</label>
             <input type="password" v-model="password" class="form-control" id="password" placeholder="Password">
           </div>
-          <button class="btn btn-primary">Submit</button>
+          <button class="btn btn-primary" v-bind:class="{
+            'disabled': disabled,
+          }">Submit</button>
         </form>
       </div>
     </div>
@@ -26,13 +28,28 @@
 export default {
   data() {
     return {
-      email: '',
-      password: ''
+      email: 'example@example.com',
+      password: '12345678',
+      disabled: false,
     }
   },
   methods: {
     login() {
-      console.log('trying to login');
+      this.disabled = true;
+      this.axios.post('/api/login', {
+        grand_type: 'password',
+        client_id: '98ca60fd-34df-478f-95f6-6b7bf06ccbbb',
+        client_secret: 'L0E3XkUpZ5CYVbmqaiU9OKHHTB0m0zIPOnNbke88',
+        username: this.email,
+        password: this.password,
+      })
+        .then(response => {
+          console.log(response.data);
+          this.disabled = false;
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 }
